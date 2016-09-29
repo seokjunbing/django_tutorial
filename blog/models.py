2469473fc4b3b376532fda89ffecda5dd051526a
seokjunbing <sjun.bing@gmail.com>
2016-09-29 18:18:08 -0400

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User') # this is a link to another model
+    title = models.CharField(max_length=200) # this is how you define text with a limited number of characters
+    text = models.TextField() # this is for long text without a limit
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
